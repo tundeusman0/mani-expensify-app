2bac6dd9412ee0d0271ef5e0b37309aee8964e29
TUNDE USMAN T.MANI <tundeusman0@gmail.com>
2018-11-10 16:09:16 +0100

    removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -15,15 +15,8 @@ import { setTextFilter} from './actions/filters'
 
 const store = configureStore();
 
-
-store.dispatch(add_expenses({ description: 'water bill', amount: 3320, createdAt: 1000 }))
-store.dispatch(add_expenses({ description: 'Gas bill', amount: 6000, createdAt: 0 }))
-store.dispatch(add_expenses({ description: 'rent', amount: 30000, createdAt: 21000 }))
-
-
 const state = store.getState()
 const visibleState = getVisibleExpenses(state.expenses, state.filters)
-console.log(visibleState)
 
 const jsx = (
     <Provider store={store}>

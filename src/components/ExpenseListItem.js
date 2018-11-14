51d8bd7cd77d3caac36474a039bd8ad1b9e0cff1
TUNDE USMAN T.MANI <tundeusman0@gmail.com>
2018-11-14 13:19:47 +0100

    created Naira sign format

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,13 +3,34 @@ import {Link} from 'react-router-dom';
 import moment from 'moment';
 import numeral from 'numeral';
 
+numeral.register('locale', 'Ng', {
+    delimiters: {
+        thousands: ',',
+        decimal: '.'
+    },
+    abbreviations: {
+        thousand: 'k',
+        million: 'm',
+        billion: 'b',
+        trillion: 't'
+    },
+    ordinal: function (number) {
+        return number === 1 ? 'er' : 'ème';
+    },
+    currency: {
+        symbol: '₦'
+    }
+});
+
+numeral.locale('Ng');
+
 const ExpenseListItem = ({id,description, amount, createdAt}) =>(
     <div>
         <Link to={`/editPage/${id}`}>
             <h3>{description}</h3>  
         </Link>
         <p>
-        {numeral(amount/ 100).format('#0,0.00')}
+            {numeral(amount / 100).format('$0,0.00')}
         -
         {moment(createdAt).format('MMM Do,YYYY')}
         </p>
